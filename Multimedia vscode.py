aa74e190b2156c45a0559e69927c341031240086
nindyawidyaazizah <145946548+nindyawidyaazizah@users.noreply.github.com>
2024-08-31 11:51:41 +0700

    Add files via upload

---
+++ Multimedia vscode.py
@@ -0,0 +1,33 @@
+print("Hello World!")
+print("Belajar Python dari Nol")
+nama = "python pemula"
+print("Hello"); print("World"); print("Tutorial Python untuk Pemula")
+nama_depan = "python"; nama_belakang = "pemula"
+udul = "Belajar Pemrograman Python sampai Bisa"penulis = 'python pemula'
+judul = "Belajar Dasa-dasar Python"
+Judul = "Belajar Membuat Program Python"
+Menurut rekomendasi style guide Google, berikut ini contoh penulisan case yang
+disarankan:
+## Snake Case digunakan pada:
+module_name, package_name, method_name, function_name , global_var_name,
+instance_var_name, function_parameter_name, local_var_name.
+## Camel Case digunakan Pada: ClassName, ExceptionName
+## Kebab Case digunakan pada: btn-primary
+## ALL CAPS digunakan Pada: GLOBAL_CONSTANT_NAME
+if username == 'pythonpemula':
+print("Selamat Datang Admin")
+print("Silakan ambil tempat duduk")
+variabel_ku = "ini isi variabel"
+variabel2 = 20
+print variabel_ku
+print variabel2
+nama, _nama, namaKu, nama_variabel
+__nama, n2, nilai1.
+>>> nama = "python"
+>>> print nama
+python
+>>> del(nama)
+>>> print nama
+Traceback (most recent call last):
+File "<stdin>", line 1, in <module>NameError: name'nama'is not defined
+>>>
